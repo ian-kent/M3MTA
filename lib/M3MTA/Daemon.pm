2da873e4f318fd58583c773e3286a9f56a9f77db
Ian Kent <email@iankent.co.uk>
2013-06-13 23:19:35 +0100

    Cleaned up Daemon class

--- lib/M3MTA/Daemon.pm
+++ lib/M3MTA/Daemon.pm
@@ -17,76 +17,36 @@ has 'stderr' => ( is => 'rw' );
 sub start {
 	my ($self, %args) = @_;
 
-	if(-e $self->pidfile) {
-	    my $realpid = $self->read_pid_file;
-
-	    my $existing = kill 0, $realpid;
-	    die($self->name . " is already running") if $existing;
-
-	    my $unlinked = unlink $self->pidfile;
-	    die("pidfile '" . $self->pidfile . "'' found but process doesn't exist, pidfile delete failed") if !$unlinked;
-	}
-
-	# Start in daemon mode
-	my $pid = 0;
 	if($self->daemon) {
-		#TODO?
-	    #mkdir $self->pid_dir if !-d $self->pid_dir;
+		# Exit or clean up pid file
+		$self->check_pid;
 
-	    # Fork a child process
-	    $pid = fork;
-	    if(!defined $pid || $pid < 0) {
-	        die("Failed to fork process");
-	    }
-	}
+		# Fork
+		my $pid = $self->fork;
 
-	# Exit this if we're the parent process
-	$pid and exit;
+		# Exit this if we're the parent process
+		$pid and exit;
 
-	if($self->daemon) {
 	    # Set our session ID
 	    setsid;
-	    $pid = fork;
+
+	    # Fork again
+	    $pid = $self->fork;
 	    exit 0 if $pid;
-	    exit 1 if not defined $pid;
 
 	    # chdir to / (its safe, and lets system unmount things)
 	    chdir '/' or die $!;
 
 	    # Set our umask
 	    umask(0);
+
+	    $self->set_filehandles;
+
+	    # Write a new pidfile
+	    $self->write_pidfile;
 	}
 
-	if($self->daemon) {
-	    # Close unwanted filehandles
-	    close *STDIN;
-	    close *STDERR;
-	    close *STDOUT;
-
-		open STDIN, '<', '/dev/null' or die "Unable to open STDIN to /dev/null: $!";
-	    
-	    if($self->stdout) {
-			open STDOUT, '>>', $self->stdout or die "Unable to open STDOUT to log file '" . $self->stdout . "': $!";
-	    } else {
-	    	open STDOUT, '<', '/dev/null' or die "Unable to open STDOUT to /dev/null: $!";
-	    }
-
-	    if($self->stderr) {
-	    	open STDERR, '>>', $self->stderr or die "Unable to open STDERR to log file '" . $self->stderr . "': $!";
-	    } else {
-	        open STDERR, '<', '/dev/null' or die "Unable to open STDERR to /dev/null: $!";
-	    }
-
-        *STDOUT->autoflush;
-        *STDERR->autoflush;
-	}
-
-	if($self->pidfile) {
-	    open my $fh, ">", $self->pidfile or die "Unable to write pidfile " . $self->pidfile;
-	    print $fh $$;
-	    close $fh;
-	}
-
+	die("No begin callback") if !$args{begin};
 	$args{begin}->();
 }
 
@@ -96,14 +56,7 @@ sub stop {
 	my ($self) = @_;
 
 	if(-e $self->pidfile) {
-        my $realpid = $self->read_pid_file;
-        
-        my $cnt = kill 9, $realpid;
-        die("Failed to kill process $realpid") if !$cnt;
-
-        my $unlinked = unlink $self->pidfile;
-        die("Failed to delete pidfile " . $self->pidfile) if !$unlinked;
-
+        $self->kill_processes;
         exit 0;
     } else {
         die($self->pidfile . " not found");
@@ -112,13 +65,96 @@ sub stop {
 
 #------------------------------------------------------------------------------
 
-sub read_pid_file {
+sub check_pid {
+	my ($self) = @_;
+
+	if(-e $self->pidfile) {
+	    my $realpid = $self->read_pidfile;
+
+	    my $existing = kill 0, $realpid;
+	    die($self->name . " is already running") if $existing;
+
+	    my $unlinked = unlink $self->pidfile;
+	    die("pidfile '" . $self->pidfile . "'' found but process doesn't exist, pidfile delete failed") if !$unlinked;
+	}
+}
+
+#------------------------------------------------------------------------------
+
+sub read_pidfile {
 	my ($self) = @_;
 
     open my $fh, "<", $self->pidfile or die "Unable to open pidfile " . $self->pidfile;
     my $realpid = <$fh>;
     close $fh;
     return $realpid;
+}
+
+#------------------------------------------------------------------------------
+
+sub write_pidfile {
+	my ($self) = @_;
+
+	if($self->pidfile) {
+	    open my $fh, ">", $self->pidfile or die "Unable to write pidfile " . $self->pidfile;
+	    print $fh $$;
+	    close $fh;
+	}
+}
+
+#------------------------------------------------------------------------------
+
+sub fork {
+	my ($self) = @_;
+
+	my $pid = fork;
+    if(!defined $pid || $pid < 0) {
+        die("Failed to fork process");
+    }
+
+    return $pid;
+}
+
+#------------------------------------------------------------------------------
+
+sub set_filehandles {
+	my ($self) = @_;
+
+	# Close unwanted filehandles
+    close *STDIN;
+    close *STDERR;
+    close *STDOUT;
+
+	open STDIN, '<', '/dev/null' or die "Unable to open STDIN to /dev/null: $!";
+    
+    if($self->stdout) {
+		open STDOUT, '>>', $self->stdout or die "Unable to open STDOUT to log file '" . $self->stdout . "': $!";
+    } else {
+    	open STDOUT, '<', '/dev/null' or die "Unable to open STDOUT to /dev/null: $!";
+    }
+
+    if($self->stderr) {
+    	open STDERR, '>>', $self->stderr or die "Unable to open STDERR to log file '" . $self->stderr . "': $!";
+    } else {
+        open STDERR, '<', '/dev/null' or die "Unable to open STDERR to /dev/null: $!";
+    }
+
+    *STDOUT->autoflush;
+    *STDERR->autoflush;
+}
+
+#------------------------------------------------------------------------------
+
+sub kill_processes {
+	my ($self) = @_;
+
+	my $realpid = $self->read_pidfile;
+        
+    my $cnt = kill 9, $realpid;
+    die("Failed to kill process $realpid") if !$cnt;
+
+    my $unlinked = unlink $self->pidfile;
+    die("Failed to delete pidfile " . $self->pidfile) if !$unlinked;
 }
 
 #------------------------------------------------------------------------------
